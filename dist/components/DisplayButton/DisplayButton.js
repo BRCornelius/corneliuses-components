4cb03e8e1f5855fbaa2e89cbb11cb9295bbd6624
Cornelius <bcornelius@iseatz.com>
2021-11-11 11:45:31 -0600

    updates import of Link

--- dist/components/DisplayButton/DisplayButton.js
+++ dist/components/DisplayButton/DisplayButton.js
@@ -7,14 +7,19 @@ exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
+var _reactRouterDom = require("react-router-dom");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const DisplayButton = _ref => {
   let {
     buttonLabel,
-    children
+    children,
+    value
   } = _ref;
-  return /*#__PURE__*/_react.default.createElement("div", null, children, /*#__PURE__*/_react.default.createElement("button", null, buttonLabel));
+  return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement(_reactRouterDom.Link, {
+    to: "/".concat(value)
+  }, children, /*#__PURE__*/_react.default.createElement("button", null, buttonLabel)));
 };
 
 var _default = DisplayButton;
--- src/lib/components/DisplayButton/DisplayButton.jsx
+++ src/lib/components/DisplayButton/DisplayButton.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const DisplayButton = ({buttonLabel, children}) => <div>
-    {children}
-    <button>
-        {buttonLabel}
-    </button>
+const DisplayButton = ({buttonLabel,children,value}) => <div>
+    <Link to={`/${value}`}>
+        {children}
+        <button>
+            {buttonLabel}
+        </button>
+    </Link>
 </div>;
 
 export default DisplayButton;
