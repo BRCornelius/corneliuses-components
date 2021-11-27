944d1ff846d721bb3fb2a3d4f2d28fb781b34dff
Cornelius <bcornelius@iseatz.com>
2021-11-26 21:56:40 -0600

    updates exports

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -1,8 +1,10 @@
 import Contact from "./Contact/Contact";
 import ContactForm from "./Contact/ContactForm";
+import DisplayButtonFunctional from "./DisplayButtonFunctional/DisplayButtonFunctional";
 import DisplayButtonLink from "./DisplayButtonLink/DisplayButtonLink";
 import NavigationHeader from "./NavigationHeader/NavigationHeader";
 import SocialFooter from "./SocialFooter/SocialFooter";
-import withHover from "./Hover/withHover";;
+import withHover from "./Hover/withHover";
+import withVideoDisplay from "./VideoDisplay/withVideoDisplay";
 
-export { Contact, ContactForm, DisplayButtonLink, NavigationHeader, SocialFooter, withHover };
+export { Contact, ContactForm, DisplayButtonFunctional, DisplayButtonLink, NavigationHeader, SocialFooter, withHover, withVideoDisplay };
